b1f701f828321d34fa90d681d883b25b8fdbebee
Susan K <kralsusa@yahoo.com>
2019-07-10 21:01:31 -0700

    my mad lib in progress

---
+++ app.js
@@ -0,0 +1,14 @@
+const nounInput = document.getElementById('noun');
+const adjectiveInput = document.getElementById('adjective');
+const learnInput = document.getElementById('learn');
+
+// Action to take on user event
+function updateResult(){
+result.textContent = nounInput.value;
+}
+function updateResult(){
+    result.textContent = adjectiveInput.value;
+    }
+    function updateResult(){
+        result.textContent = learnInput.value;
+        }
