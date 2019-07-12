559c5cc4c9393d9d437d13e43a2067bcef5f8993
Susan K <kralsusa@yahoo.com>
2019-07-11 18:21:12 -0700

    update

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-const nounInput = document.getElementById('noun');
-const adjectiveInput = document.getElementById('adjective');
-const learnInput = document.getElementById('learn');
+const nounInput = document.getElementById('noun1input');
+const adjectiveInput = document.getElementById('adjective1input');
+const learnInput = document.getElementById('learn1input');
+
+const nounResult = document.getElementById('noun1Result');
+const noun2Result = document.getElementById('noun1Result');
+const adjectiveResult = document.getElementById('adjective1Result');
+const learnResult = document.getElementById('learn1Result');
+
+
 
 // Action to take on user event
 function updateResult(){
-result.textContent = nounInput.value;
-}
-function updateResult(){
-    result.textContent = adjectiveInput.value;
-    }
-    function updateResult(){
-        result.textContent = learnInput.value;
-        }
+nounResult.textContent = nounInput.value}
 
-        function myFunction() {
-            var x = document.getElementById("myStory");
-            if (x.style.display === "none") {
-              x.style.display = "block";
-            } else {
-              x.style.display = "none";
-            }
-          }
+function updateResult(){
+  noun2Result.textContent = nounInput.value}
+
+  function updateResult(){
+    adjectiveResult.textcontent = adjectiveInput.value}
+
+    function updateResult(){
+      learnResult.textcontent = learnInput.value}
+
+
