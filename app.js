b38c931032de182de050ca21270e6c99bf77e54a
Susan K <kralsusa@yahoo.com>
2019-07-13 22:33:37 -0700

    modified story text

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-// Action to take on user event
+// Action to take on user event of pushing the button
 function updateResult() {
 
 
-const nounInput = document.getElementById('nounInput');
-const adjectiveInput = document.getElementById('adjectiveInput');
-const learnInput = document.getElementById('learnInput');
+    const nounInput = document.getElementById('nounInput');
+    const adjectiveInput = document.getElementById('adjectiveInput');
+    const learnInput = document.getElementById('learnInput');
 
-const nounResult = document.getElementById('nounResult');
-const adjectiveResult = document.getElementById('adjectiveResult');
-const learnResult = document.getElementById('learnResult');
+    const nounResult = document.getElementById('nounResult');
+    const adjectiveResult = document.getElementById('adjectiveResult');
+    const learnResult = document.getElementById('learnResult');
 
-document.getElementById("myStory").setAttribute('style', 'visibility: visible');
+    document.getElementById("myStory").setAttribute('style', 'visibility: visible');
 
-nounResult.innerText = nounInput.value;
-adjectiveResult.innerText = adjectiveInput.value;
-learnResult.innerText = learnInput.value;
+    nounResult.innerText = nounInput.value;
+    adjectiveResult.innerText = adjectiveInput.value;
+    learnResult.innerText = learnInput.value;
 
 }
 
